cfa9d56d26bd956e0b6a31f7e6cc5425ec317a25
Taras Ilkiv <tilkiv@falconsoft-ltd.com>
2014-03-05 12:19:07 +0200

    Changed data Model

--- RTServer/Controllers/ValuesController.cs
+++ RTServer/Controllers/ValuesController.cs
@@ -10,17 +10,21 @@ namespace RTServer.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        public IEnumerable<Record> Get()
+        public IEnumerable<Customer> Get()
         {
-            var list = new List<Record>();
+            var list = new List<Customer>();
 
             for (int i = 0; i < 10; i++)
             {
-                list.Add(new Record
+                list.Add(new Customer
                 {
-                    RecordKey = i,
-                    Value = "Value "+ i,
-                    Description = "Description : " + DateTime.Now
+                    OrderId = i * 123,
+                    CustomerId = "Customer "+ i,
+                    EmployeeId = i,
+                    Freight = (double)i/32,
+                    OrderDate = DateTime.Now,
+                    ShipAdress = "Lviv, Street " + i *2 ,
+                    ShipName = "SomeName"
                 });
             }
             return list;
@@ -48,12 +52,51 @@ namespace RTServer.Controllers
         }
     }
 
-    public class Record
+    public class Customer
     {
-        public int RecordKey { get; set; }
+        public int OrderId { get; set; }
 
-        public string Value { get; set; }
+        public string CustomerId { get; set; }
 
-        public string Description { get; set; }
+        public int EmployeeId { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public double Freight { get; set; }
+
+        public string ShipName { get; set; }
+
+        public string ShipAdress { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            var record = obj as Customer;
+
+            if (this.OrderId != record.OrderId ||
+                this.CustomerId != record.CustomerId ||
+                this.EmployeeId != record.EmployeeId ||
+                this.OrderDate != record.OrderDate ||
+                this.Freight != record.Freight ||
+                this.ShipName != record.ShipName ||
+                this.ShipAdress != record.ShipAdress)
+                return false;
+            return true;
+        }
+
+        public Customer Clone()
+        {
+            var temp = new Customer()
+            {
+                OrderId = OrderId,
+                CustomerId = CustomerId,
+                EmployeeId = EmployeeId,
+                OrderDate = OrderDate,
+                Freight = Freight,
+                ShipAdress = ShipAdress,
+                ShipName = ShipName
+            };
+            return temp;
+        }
     }
 }
