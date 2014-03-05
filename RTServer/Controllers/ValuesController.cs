df8a57b4e7a8018b0375a37984b29b1ad56e535e
Yaroslav Dubaniovsky <ydubaniovsky@falconsoft-ltd.com>
2014-03-05 12:22:55 +0200

    smth Changed

--- RTServer/Controllers/ValuesController.cs
+++ RTServer/Controllers/ValuesController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Web.Http;
 
 namespace RTServer.Controllers
@@ -12,19 +14,37 @@ namespace RTServer.Controllers
         // GET api/values
         public IEnumerable<Record> Get()
         {
-            var list = new List<Record>();
+            //var list = new List<Record>();
 
-            for (int i = 0; i < 10; i++)
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    list.Add(new Record
+            //    {
+            //        RecordKey = i,
+            //        Value = "Value "+ i,
+            //        Description = "Description : " + DateTime.Now
+            //    });
+            //}
+            //return list;
+            var outputDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase);
+            var d = Path.Combine(outputDirectory, "Customers.txt");
+            var d1 = new Uri(d).LocalPath;
+            using (var sr = new StreamReader(Path.Combine(outputDirectory,"Customers.txt")))
             {
-                list.Add(new Record
+                while (!sr.EndOfStream)
                 {
-                    RecordKey = i,
-                    Value = "Value "+ i,
-                    Description = "Description : " + DateTime.Now
-                });
+                    var strLine = sr.ReadLine();
+                    var data = strLine.Split('\t');
+
+                    foreach (var s in data)
+                    {
+
+                    }
+                }
             }
-            return list;
+            return null;
         }
+
 
         // GET api/values/5
         public string Get(int id)
